5605597b2b43a7464b0a84dbc7ed42c023dfd569
Paul Bakker <paul@theorangedots.com>
2020-08-18 10:01:25 +0200

    fix Compatibility with Servoy >= 2019.12

--- src/com/tod/servoy/plugins/queueing/server/Plugin.java
+++ src/com/tod/servoy/plugins/queueing/server/Plugin.java
@@ -11,30 +11,36 @@ import com.servoy.j2db.server.shared.ApplicationServerRegistry;
 import com.servoy.j2db.util.Debug;
 
 public class Plugin implements IServerPlugin {
+	private int attemptCount = 0;
 	
 	@Override
 	public void load() throws PluginException {}
 
 	@Override
 	public void initialize(final IServerAccess access) throws PluginException {
-			int delay = ApplicationServerRegistry.get().isDeveloperStartup() ? 30 : 0;
-		
-			//Delayed init as the plugin is getting loaded during the server startup
-			try {
-				access.getExecutor().schedule(new Runnable() {
-					
-					@Override
-					public void run() {
-						if (ApplicationServerRegistry.waitForApplicationServerStarted()) {
-							ConfigHandler.init(access);
-						}
+			int initialDelay = ApplicationServerRegistry.get().isDeveloperStartup() ? 30 : 0;
+			
+			Runnable runner = new Runnable() {
+				@Override
+				public void run() {
+					if (!ApplicationServerRegistry.get().isStarting()) {
+						ConfigHandler.init(access);
+					} else if (attemptCount == 120) {
+						Debug.error("Failed to initializing plugin: server indicating it hasn't started");
+					} else {
+						attemptCount++;
+
+						access.getExecutor().schedule(this, 1, TimeUnit.SECONDS);
 					}
-					
-				}, delay, TimeUnit.SECONDS);
+				}
+			};
+			
+			//Delayed init as the plugin is getting loaded during the server startup and the plugin needs solutions available
+			try {
+				access.getExecutor().schedule(runner, initialDelay, TimeUnit.SECONDS);				
 			} catch (Exception e) {
 				Debug.error("Failure initializing plugin", e);
 			}
-
 	}
 	
 	@Override
